728c4a0df8ae6b28bca63e73d53de485f6345331
Nastya880 <oidmakeevy@gmail.com>
2023-06-10 10:19:10 +0300

    learn in JS

--- pages-js/script.js
+++ pages-js/script.js
@@ -1,2 +1,23 @@
 "use strict"; //в современных браузерах  1ая строка  - вкл строгого режима
-alert('text!');
+// alert('text!');
+
+// let str = "abcde";
+// alert("длина строки = " + str.length);
+
+// // проверка многострочности
+// let a = `hello
+// world
+// !
+// It's многострочность`;
+// alert(a);
+
+// // шаблонная строка(с переменными)
+// age = 22;
+// alert(`I have ${age} years old`);
+
+// let num = "12px";
+// alert(parseInt(num));
+
+
+let name = prompt('Ваше имя?');
+alert(`You input ${name}`);
